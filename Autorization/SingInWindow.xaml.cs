5e99d0a3730de335b93c162ad268a0cf0447e5e0
Evgeny <ololo128666@gmail.com>
2025-03-04 01:38:05 +0500

    Добавлено новое окно

---
+++ Autorization/SingInWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Autorization
+{
+    public partial class SingInWindow : Window
+    {
+        public SingInWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
