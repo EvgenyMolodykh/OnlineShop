a241f90d2788e247cbe2abadd76eecd745c514aa
Evgeny <ololo128666@gmail.com>
2025-03-04 01:20:05 +0500

    Реализовал окно регистрации Реализовал форму регистрации Реализовал логику вызова окна регистрации

---
+++ Autorization/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Autorization
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Register_Button.Click += Register_Button_Click; 
+        }
+
+        private void Register_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var RegistrationWindow = new RegistrationWindow();
+            RegistrationWindow.Show();
+        }
+    }
+}
