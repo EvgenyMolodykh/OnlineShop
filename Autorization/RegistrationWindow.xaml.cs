e4856e9a2ac08b0e7ab379911519d8012372ae7b
Evgeny <ololo128666@gmail.com>
2025-03-06 02:20:20 +0500

    Валидация данных для регитсрации формы

--- Autorization/RegistrationWindow.xaml.cs
+++ Autorization/RegistrationWindow.xaml.cs
@@ -12,8 +12,41 @@ namespace Autorization
 
         private void Registration_Button_Click(object sender, RoutedEventArgs e)
         {
-            Close();
+            
+            var login = RegLogin_TextBox.Text;
+            var password = RegPassword_TextBox.Text;
+            var confirmPassword = ConfirmPassword_TextBox.Text;
+
+            if (string.IsNullOrEmpty(login))
+            {
+                MessageBox.Show("Логин не может быть пустым");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Пароль не может быть пустым");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(confirmPassword))
+            {
+                MessageBox.Show("Поле подтверждения пароля не может быть пустым");
+                return;
+            }
+
+            if (password != confirmPassword)
+            {
+                MessageBox.Show("Пароли не совпадают");
+                return;
+            }
+
+            RegisterUser();
         }
 
+        private void RegisterUser()
+        {
+            MessageBox.Show("Вы успешно зарегестрировались");
+        }
     }
 }
