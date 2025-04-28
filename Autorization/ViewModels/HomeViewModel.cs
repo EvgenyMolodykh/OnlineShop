b7f1c62fb513c9710a222f3120aa0288a6e05706
Evgeny <ololo128666@gmail.com>
2025-04-29 00:50:15 +0500

    +

--- Autorization/ViewModels/HomeViewModel.cs
+++ Autorization/ViewModels/HomeViewModel.cs
@@ -1,9 +1,38 @@
-﻿using System.ComponentModel;
+﻿using Autorization.Models;
+using Autorization.Repository;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Autorization.ViewModels
 {
     public class HomeViewModel : INotifyPropertyChanged
     {
+        private List<DayForecastModel> forecastsDays;
+
+        public List<DayForecastModel> ForecastsDays
+        {
+            get { return forecastsDays; }
+            set
+            {
+                forecastsDays = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public HomeViewModel()
+        {
+            ForecastsDays = WeatherDataStorage.GetAll();
+        }
+
         public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string prop= "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+
+        
     }
 }
