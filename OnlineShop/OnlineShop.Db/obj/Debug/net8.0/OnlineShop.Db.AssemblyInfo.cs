a6ae28e1e66e829356ad4eb30efa822f952afcf1
Evgeny <ololo128666@gmail.com>
2025-05-05 19:23:18 +0500

    0

--- OnlineShop/OnlineShop.Db/obj/Debug/net8.0/OnlineShop.Db.AssemblyInfo.cs
+++ OnlineShop/OnlineShop.Db/obj/Debug/net8.0/OnlineShop.Db.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineShop.Db")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96898a8100fb369359b2553766955ed23bd60ca6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35934518da61cec29f6d59c8090fd051f3227e3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineShop.Db")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineShop.Db")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
